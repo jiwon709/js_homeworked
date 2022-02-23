65091dd2e7e30616c34400c9af2a100be45cd9fb
jiwon709 <jimong8702@naver.com>
2022-02-23 23:47:44 +0900

    Q1, Q2 OK

--- question2.js
+++ question2.js
@@ -1,3 +1,24 @@
 //[객체] car 객체를 생성 (속성은 2가지로 model 과 name 을 임의로 입력, 
 //메소드로 getFullName 을 만들며 메소드의 결과는 
 //[model : name] 콘솔 로그(console.log())로 출력)
+
+// const car = {
+//     model : 'GENESIS',
+//     name : 'G90',
+//     getFullName : function() {
+//         return '${this.model} : ${this.name}'
+//     }
+// }
+
+function user(modelPut, namePut) {
+    this.model = modelPut;
+    this.name = namePut;
+}
+
+user.prototype.getFullName = function() {
+    return this.model + ':' + this.name;
+}
+
+const car = new user('GENESIS', 'G90');
+
+console.log(car.getFullName());
